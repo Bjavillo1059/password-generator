beebb2b66cfcae6035b7ec36af1d71c6fbeff73c
Brandon Javillo <bjavillo1059@gmail.com>
2021-07-09 22:44:48 -0700

    added changes with let statement

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,41 +1,40 @@
-var symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "+"];
-var numbers = 128;
+var symbols = "!@#$%^&*()_-+=";
+var numbers = "0123456789";
+var letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+	var password = generatePassword();
+	var passwordText = document.querySelector("#password");
+	
+	passwordText.value = password;	
 }
-
-
 
 function generatePassword() {
 	var length = window.prompt("choose a length of numbers for your password");
-		console.log(typeof length); // validate it as a string not to convert to a number
-		if (length < 8  || length > 128) {
-			window.alert("you must chose again")
-			return;
-		}
+	console.log(typeof length); // validate it as a string not to convert to a number
+	if (length < 8  || length > 128) {
+		window.alert("you must chose again")
+		return;
+	}
+	
+	// varibales to select symbols, numbers, letters	
 	var symbol = window.confirm("would you like to use symbols?");
 	var number = window.confirm("would you like to use a number?");
 	var letter = window.confirm("would you like to use letters");
-	// var number = window.confrim (add more info later)
 	var passString = ""
 
+	let charInput = length
+	letter ? (charInput += letters) : "";
+	symbol ? (charInput += symbols) : "";
+	number ? (charInput += numbers) : "";
+
 	for (let i = 0; i < length; i++) {
-		var index = Math.floor(Math.random() * symbols.length);
-  		var random = symbols[index]; // index generated from previous line
-		passString += random
-	}
-	for (let i = 0; i < length; i++) {
-		var index = Math.floor(Math.random() * numbers.length);
-  		var random = numbers[index]; // index generated from previous line
+		var index = Math.floor(Math.random() * charInput.length);
+  		var random = charInput[index]; // index generated from previous line
 		passString += random
 	}
 	
@@ -44,4 +43,5 @@ function generatePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+	alert("Remember to copy your password when generated");
 
